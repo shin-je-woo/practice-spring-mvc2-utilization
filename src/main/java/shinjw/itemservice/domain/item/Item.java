80e7e014c3337a9afa78b8642c42810c180ca99e
shin.jewoo <shinjw0926@naver.com>
2023-03-12 15:17:39 +0900

    검증2 Bean Validation - 프로젝트 준비

--- src/main/java/shinjw/itemservice/domain/item/Item.java
+++ src/main/java/shinjw/itemservice/domain/item/Item.java
@@ -1,7 +1,11 @@
 package shinjw.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 
@@ -9,8 +13,16 @@ import java.util.List;
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min=1000, max=1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
 
