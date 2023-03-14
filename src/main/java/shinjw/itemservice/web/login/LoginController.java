b60e512361307aebc160f6e3d9801167b6175de7
shin.jewoo <shinjw0926@naver.com>
2023-03-14 23:26:39 +0900

    로그인 처리 - 서블릿 필터를 이용한 요청 로그, 인증 체크

--- src/main/java/shinjw/itemservice/web/login/LoginController.java
+++ src/main/java/shinjw/itemservice/web/login/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import shinjw.itemservice.domain.login.LoginService;
 import shinjw.itemservice.domain.member.Member;
 import shinjw.itemservice.web.SessionConst;
@@ -75,7 +76,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult.getAllErrors());
@@ -97,6 +98,32 @@ public class LoginController {
 
         return "redirect:/";
     }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(required = false) String redirectURL,
+                          HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult.getAllErrors());
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.longin(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "ID 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+        // 세션이 있으면 있는 세션 반환, 없으면 신규 세션 반환
+        HttpSession session = request.getSession(true);
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:" + redirectURL;
+    }
+
 
     //@PostMapping("/logout")
     public String logoutV1(HttpServletResponse response) {
