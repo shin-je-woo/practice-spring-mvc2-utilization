a0964aac15086cde1b13c0f7ced0f607b92f3a4d
shin.jewoo <shinjw0926@naver.com>
2023-03-20 21:50:12 +0900

    스프링 타입 컨버터 - Converter와 ConversionService

---
+++ src/test/java/shinjw/itemservice/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,30 @@
+package shinjw.itemservice.typeconverter.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+import shinjw.itemservice.typeconverter.type.IpPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+
+        //사용
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        String ipPortString = conversionService.convert(new IpPort("127.0.0.1", 8080), String.class);
+        assertThat(ipPortString).isEqualTo("127.0.0.1:8080");
+    }
+}
